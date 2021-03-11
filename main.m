e48378c2938786ce5696088c482ebf5736c4cd94
robitron <robby.garner@gmail.com>
2021-03-10 22:06:57 -0500

    Add files via upload

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  JFRED
+//
+//  Created by Robby Garner on Mon Feb 23 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
